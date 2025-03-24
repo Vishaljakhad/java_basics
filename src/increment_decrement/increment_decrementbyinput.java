072d213fa87899609c01c1b4fe808da93ba87219
pritijabit23 <priti@jabitsoft.com>
2025-03-24 06:22:45 +0530

    24 march

---
+++ src/increment_decrement/increment_decrementbyinput.java
@@ -0,0 +1,12 @@
+package increment_decrement;
+
+public class increment_decrementbyinput {
+
+    public static void main(String[] args) {
+
+        //take inputs
+        int age = 25;
+        String Canigogoa = age >= 24 ? "yes" : "no";
+        System.out.println(Canigogoa);
+    }
+}
--- src/operators/ternary_operator.java
+++ src/operators/ternary_operator.java
@@ -14,6 +14,16 @@ public static void main(String[] args) {
     String canIvote =  age >= 18 ? "yes, you can vote" : "No, you can't vote" ;
 
     System.out.println(canIvote);
+
+
+   // question : Find the largest and smallest number from n1,n3,n3
+     int n1 = 2, n2= 9, n3 =-11;
+
+     int largest =   (n1 >= n2) ? ((n1>=n3)  ? n1 : n3 ) : ((n2 >= n3) ? n2: n3);
+    int smallest =   (n1 <= n2) ? ((n1<=n3)  ? n1 : n3 ) : ((n2 <= n3) ? n2: n3);
+    System.out.println(" largest number:" + largest );
+    System.out.println(" smallest number:" + smallest );
+
 }
 
 
